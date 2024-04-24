4346df8d74acbd5a8d25dfc945c7887fdd8e4ab9
Ana Maria <133285666+aninha1103@users.noreply.github.com>
2024-04-24 18:47:40 -0300

    Add files via upload

--- ex011/ex011.js
+++ ex011/ex011.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const mainItems = document.querySelectorAll('.main-item');
 
-    // Adiciona um evento de clique a todos os itens principais
+    // adiciona um evento de clique a todos os itens principais
     mainItems.forEach(function(item) {
         item.addEventListener('click', function() {
             const subMenu = this.querySelector('.sub-menu');
-            
-            // Alternar classe 'expanded' para realçar o item principal clicado
-            this.classList.toggle('expanded');
 
-            // Alternar classe 'visible' para mostrar ou ocultar a subseção correspondente
+            // alternar classe 'visible' para mostrar ou ocultar o submenu correspondente
             if (subMenu) {
                 subMenu.classList.toggle('visible');
             }
